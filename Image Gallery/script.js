853de57792c0da665528d75e2e3f12af743ab701
Muhammad Suffiyan Rafi <56222285+SufyanCh632@users.noreply.github.com>
2026-09-10 22:52:40 +0500

    script.js

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -1,1 +1,28 @@
+window.onload = function()
+{
+    getImageGalleryReady();
 
+    document.getElementById("crossbtn").onclick = function()
+    {
+        document.getElementById("modal").style.display = "none";
+    }
+}
+
+function getImageGalleryReady()
+{
+    var img = document.getElementsByClassName("main-img");
+
+    for(var i = 0; i < img.length; i++)
+    {
+        img[i].onclick = function()
+        {
+            openModal(this.src);
+        }
+    }
+}
+
+function openModal(src)
+{
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("modalImg").src = src;
+}
